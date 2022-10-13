cbc44ed192d40f3d997f3ae9bc544739df40a897
Ilya Zahorodnii <70091202+1Alusha1@users.noreply.github.com>
2022-10-13 11:43:18 +0300

    add  eslintrc config

---
+++ coures-work/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "ES6": true
+    },
+    "extends": "plugin:vue/vue3-essential",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
